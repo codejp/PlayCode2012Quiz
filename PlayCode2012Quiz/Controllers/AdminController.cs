a7dcd301c021c743d5146268e750c09728fee2df
jsakamoto <junichi.jp@gmail.com>
2012-07-26 22:52:09 +0900

    Implemented Admin page.

--- PlayCode2012Quiz/Controllers/AdminController.cs
+++ PlayCode2012Quiz/Controllers/AdminController.cs
@@ -3,14 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PlayCode2012Quiz.Models;
 
 namespace PlayCode2012Quiz.Controllers
 {
     public class AdminController : Controller
     {
+        public PlayCode2012QuizDB DB { get; set; }
+
+        public AdminController()
+        {
+            this.DB = new PlayCode2012QuizDB();
+        }
+
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            return View(this.DB);
+        }
+
+        [HttpPost]
+        public ActionResult CurrentState(int state)
+        {
+            var context = this.DB.Context.First();
+            context.CurrentState = state;
+            
+            // if change state to "3:show answer", judge to all players.
+            if (state == 3)
+            {
+                var answers = this.DB
+                    .Answers
+                    .Where(a =>a.QuestionID == context.CurrentQuestionID)
+                    .ToList();
+                var currentQuestion = this.DB.Questions.Find(context.CurrentQuestionID);
+
+                answers
+                    .ForEach(a => a.Status = 
+                        a.ChoincedOptionIndex == currentQuestion.IndexOfCorrectOption
+                        ? 2 : 3);
+            }
+            
+            this.DB.SaveChanges();
+
+            return Json(new { });
+        }
+
+        [HttpPost]
+        public ActionResult CurrentQuestion(int questionID)
+        {
+            this.DB.Context.First().CurrentQuestionID = questionID;
+            this.DB.SaveChanges();
+
+            return Json(new { });
         }
     }
 }
---
+++ PlayCode2012Quiz/Views/Admin/Admin.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    var updateViewState = function () {
+        var currentStateVal = parseInt($("#currentState").val());
+        if (currentStateVal != 0)
+            $("#currentQuestion").attr("disabled", "disabled");
+        else
+            $("#currentQuestion").removeAttr("disabled");
+    };
+
+    updateViewState();
+
+    $("#currentState").live("change", function () {
+        $.post("/Admin/CurrentState", { "state": $(this).val() });
+        updateViewState();
+    });
+
+    $("#currentQuestion").live("change", function () {
+        $.post("/Admin/CurrentQuestion", { "questionID": $(this).val() });
+        updateViewState();
+    });
+});
