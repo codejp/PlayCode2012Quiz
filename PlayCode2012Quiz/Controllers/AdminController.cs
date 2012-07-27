1b58dc78059e7953cffbd59052aa575dd8d8168b
jsakamoto <junichi.jp@gmail.com>
2012-07-27 19:03:20 +0900

    Implemented import questions from Google form directly.

--- PlayCode2012Quiz/Controllers/AdminController.cs
+++ PlayCode2012Quiz/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using PlayCode2012Quiz.Models;
@@ -63,6 +65,53 @@ namespace PlayCode2012Quiz.Controllers
             this.DB.SaveChanges();
 
             return Json(new { });
+        }
+ 
+    
+        [HttpGet]
+        public ActionResult ImportQuestion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ImportQuestion(FormCollection f)
+        {
+            var GDbUser = ConfigurationManager.AppSettings["GData.User"];
+            var GDbPwd = ConfigurationManager.AppSettings["GData.Password"];
+
+            var questions = 
+                new GDataDB.DatabaseClient(GDbUser, GDbPwd)
+                .GetDatabase("CopyOfCode2012Quiz")
+                .GetTable<GDataQuestion>("Sheet1")
+                .FindAll()
+                .Select(r => r.Element)
+                .ToArray();
+
+            using (var db = new PlayCode2012QuizDB())
+            {
+                db.Database.ExecuteSqlCommand("TRUNCATE TABLE Questions");
+
+                var qs = questions
+                    .Select(q => new Question
+                    {
+                        Body = q.Body,
+                        Option0 = q.Option0,
+                        Option1 = q.Option1,
+                        Option2 = q.Option2,
+                        Option3 = q.Option3,
+                        Option4 = q.Option4,
+                        Option5 = q.Option5,
+                        Comment = q.Comment,
+                        IndexOfCorrectOption = int.Parse(Regex.Match(q.IndexOfCorrectOption, @"\d+").Value)
+                    })
+                    .ToList();
+                    qs.ForEach(q => db.Questions.Add(q));
+
+                db.SaveChanges();
+            }
+
+            return View();
         }
     }
 }
