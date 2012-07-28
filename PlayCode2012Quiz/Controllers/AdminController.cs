9f29820b55e910d6f6d80d4cd46b03fa79d77232
jsakamoto <junichi.jp@gmail.com>
2012-07-28 16:04:06 +0900

    Implemented impoting from Normalian's Site!

--- PlayCode2012Quiz/Controllers/AdminController.cs
+++ PlayCode2012Quiz/Controllers/AdminController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using PlayCode2012Quiz.Models;
 
 namespace PlayCode2012Quiz.Controllers
@@ -77,39 +80,69 @@ namespace PlayCode2012Quiz.Controllers
         [HttpPost]
         public ActionResult ImportQuestion(FormCollection f)
         {
-            var GDbUser = ConfigurationManager.AppSettings["GData.User"];
-            var GDbPwd = ConfigurationManager.AppSettings["GData.Password"];
+            var client = new WebClient { Encoding = Encoding.UTF8 };
+            var jsonStr = client.DownloadString(new Uri("http://codejp2012quiz.cloudapp.net/Quiz/GetList"));
 
-            var questions = 
-                new GDataDB.DatabaseClient(GDbUser, GDbPwd)
-                .GetDatabase("CopyOfCode2012Quiz")
-                .GetTable<GDataQuestion>("Sheet1")
-                .FindAll()
-                .Select(r => r.Element)
-                .ToArray();
+            var normalianQuestions = JsonConvert.DeserializeObject<List<NormalianQuestion>>(jsonStr);
+
+            var qs = normalianQuestions
+                .Select(q => new Question
+                {
+                    Body = q.QuestionSummary,
+                    Option0 = q.Answer1,
+                    Option1 = q.Answer2,
+                    Option2 = q.Answer3,
+                    Option3 = q.Answer4,
+                    Option4 = q.Answer5,
+                    Option5 = q.Answer6,
+                    Comment = q.AnswerDetail,
+                    IndexOfCorrectOption = q.AnswerNo
+                })
+                .ToList();
 
             using (var db = new PlayCode2012QuizDB())
             {
                 db.Database.ExecuteSqlCommand("TRUNCATE TABLE Questions");
 
-                var qs = questions
-                    .Select(q => new Question
-                    {
-                        Body = q.Body,
-                        Option0 = q.Option0,
-                        Option1 = q.Option1,
-                        Option2 = q.Option2,
-                        Option3 = q.Option3,
-                        Option4 = q.Option4,
-                        Option5 = q.Option5,
-                        Comment = q.Comment,
-                        IndexOfCorrectOption = int.Parse(Regex.Match(q.IndexOfCorrectOption, @"\d+").Value) - 1
-                    })
-                    .ToList();
-                    qs.ForEach(q => db.Questions.Add(q));
+                qs.ForEach(q => db.Questions.Add(q));
 
                 db.SaveChanges();
             }
+
+
+            //var GDbUser = ConfigurationManager.AppSettings["GData.User"];
+            //var GDbPwd = ConfigurationManager.AppSettings["GData.Password"];
+
+            //var questions = 
+            //    new GDataDB.DatabaseClient(GDbUser, GDbPwd)
+            //    .GetDatabase("CopyOfCode2012Quiz")
+            //    .GetTable<GDataQuestion>("Sheet1")
+            //    .FindAll()
+            //    .Select(r => r.Element)
+            //    .ToArray();
+
+            //using (var db = new PlayCode2012QuizDB())
+            //{
+            //    db.Database.ExecuteSqlCommand("TRUNCATE TABLE Questions");
+
+            //    var qs = questions
+            //        .Select(q => new Question
+            //        {
+            //            Body = q.Body,
+            //            Option0 = q.Option0,
+            //            Option1 = q.Option1,
+            //            Option2 = q.Option2,
+            //            Option3 = q.Option3,
+            //            Option4 = q.Option4,
+            //            Option5 = q.Option5,
+            //            Comment = q.Comment,
+            //            IndexOfCorrectOption = int.Parse(Regex.Match(q.IndexOfCorrectOption, @"\d+").Value) - 1
+            //        })
+            //        .ToList();
+            //        qs.ForEach(q => db.Questions.Add(q));
+
+            //    db.SaveChanges();
+            //}
 
             return View();
         }
---
+++ PlayCode2012Quiz/Models/NormalianQuestion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlayCode2012Quiz.Models
+{
+    public class NormalianQuestion
+    {
+        public string QuestionSummary { get; set; }
+        public string AnswerDetail { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public string Answer5 { get; set; }
+        public string Answer6 { get; set; }
+        public int AnswerNo { get; set; }
+    }
+}
