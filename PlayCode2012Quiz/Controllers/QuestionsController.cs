caff56d9494c3018f82be4b36fd98111059bf045
jsakamoto <junichi.jp@gmail.com>
2012-08-07 12:33:27 +0900

    Add api to publish questions.

---
+++ PlayCode2012Quiz/Controllers/QuestionsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using PlayCode2012Quiz.Models;
+
+namespace PlayCode2012Quiz.Controllers
+{
+    public class QuestionsController : ApiController
+    {
+        private PlayCode2012QuizDB db = new PlayCode2012QuizDB();
+
+        // GET api/Questions
+        [Queryable]
+        public IQueryable<Question> GetQuestions()
+        {
+            return db.Questions;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
